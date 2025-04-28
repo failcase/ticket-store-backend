67c6e01871553aa44dabce06c32944c69c5d6f1a
Rustam Astafeev <rustam@astafeev.dev>
2025-04-28 19:14:19 +0500

    add SECURE_PROXY_SSL_HEADER

--- core/settings.py
+++ core/settings.py
@@ -164,6 +164,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
